e9c218880924cc050a26280f4380941d9d271444
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2018-11-29 00:12:23 -0600

    -Added some different hat option. - Now loops through the hat array

--- CharacterCreator/MainWindow.xaml.cs
+++ CharacterCreator/MainWindow.xaml.cs
@@ -20,9 +20,60 @@ namespace CharacterCreator
     /// </summary>
     public partial class MainWindow : Window
     {
+        SolidColorBrush color = new SolidColorBrush();
+
+        string[] Hair = { "", "Sunglasses.png", "TopHat.png" };
+        int CurrentHair = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void slid_Red_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.Color = Color.FromArgb(100, Convert.ToByte(slid_Red.Value), Convert.ToByte(slid_Green.Value), Convert.ToByte(slid_Blue.Value));
+        }
+
+        private void r_Fill_Loaded(object sender, RoutedEventArgs e)
+        {
+            r_Fill.Fill = color;
+        }
+
+        private void slid_Green_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.Color = Color.FromArgb(100, Convert.ToByte(slid_Red.Value), Convert.ToByte(slid_Green.Value), Convert.ToByte(slid_Blue.Value));
+        }
+
+        private void slid_Blue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.Color = Color.FromArgb(100, Convert.ToByte(slid_Red.Value), Convert.ToByte(slid_Green.Value), Convert.ToByte(slid_Blue.Value));
+        }
+
+        private void bt_HairRight_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentHair < Hair.Length)
+            {
+                img_Hair.Source = new BitmapImage(new Uri(Hair[CurrentHair++], UriKind.Relative));
+            }
+            else
+            {
+                CurrentHair = 0;
+                img_Hair.Source = new BitmapImage(new Uri(Hair[CurrentHair++], UriKind.Relative));
+            }
+        }
+
+        private void bt_HairLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentHair < 0)
+            {
+                img_Hair.Source = new BitmapImage(new Uri(Hair[CurrentHair--], UriKind.Relative));
+            }
+            else
+            {
+                CurrentHair = Hair.Length;
+                img_Hair.Source = new BitmapImage(new Uri(Hair[CurrentHair--], UriKind.Relative));
+            }
         }
     }
 }
